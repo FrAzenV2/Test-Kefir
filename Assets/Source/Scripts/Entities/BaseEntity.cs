cde533f4f9f72a9083aff0fa5f0f1723d36f8c40
FrAzenV2 <frazenv2@ya.ru>
2022-10-11 14:18:29 +0300

    Add ship entity

---
+++ Assets/Source/Scripts/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Source.Scripts.Components;
+
+namespace Source.Scripts.Entities
+{
+    public abstract class BaseEntity
+    {
+        public abstract void OnUpdate(float deltaTime);
+        public abstract void OnFixedUpdate(float deltaTime);
+
+        protected List<UpdatableComponent> _updatableComponents = new List<UpdatableComponent>();
+        protected List<UpdatableComponent> _fixedUpdatableComponents = new List<UpdatableComponent>();
+    }
+}
---
+++ Assets/Source/Scripts/Entities/ShipEntity.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Source.Scripts.Components;
+using Source.Scripts.Configs;
+using Source.Scripts.Input;
+using UnityEngine;
+
+namespace Source.Scripts.Entities
+{
+    public class ShipEntity : BaseEntity
+    {
+        public ShipMovementComponent MovementComponent { get; private set; }
+        
+        public ShipEntity(MovementConfig movementConfig, InputState inputState)
+        {
+            MovementComponent = new ShipMovementComponent(inputState, movementConfig, Vector2.zero, Vector2.zero, 0);
+            
+            _fixedUpdatableComponents.Add(MovementComponent);
+        }
+
+        public override void OnUpdate(float deltaTime)
+        {
+            foreach (var updatableComponent in _updatableComponents)
+            {
+                updatableComponent.OnUpdate(deltaTime);
+            }
+        }
+        
+        public override void OnFixedUpdate(float deltaTime)
+        {
+            foreach (var fixedUpdatableComponent in _fixedUpdatableComponents)
+            {
+                fixedUpdatableComponent.OnUpdate(deltaTime);
+            }
+        }
+
+    }
+}
